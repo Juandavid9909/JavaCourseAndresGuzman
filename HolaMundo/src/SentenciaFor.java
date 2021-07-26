70c956fed0acb040b4025f6b9241c935cd6ed1e2
Juan David <juandavid918@gmail.com>
2021-07-26 08:45:07 -0500

    Sentencia switch vista

---
+++ HolaMundo/src/SentenciaFor.java
@@ -0,0 +1,7 @@
+public class SentenciaFor {
+    public static void main(String[] args) {
+        for(int i = 0, j = 10; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
